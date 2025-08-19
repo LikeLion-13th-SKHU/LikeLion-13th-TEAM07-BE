7ffab280df91e35a0f52b20d840813bdc8f9faf9
ckr8305 <ckr8305@naver.com>
2025-08-19 15:41:28 +0900

    Refactor(#6): 스웨거 문서 추가 및 로그인 적용

--- ie_um/src/main/java/com/example/ie_um/resource/api/ResourcesController.java
+++ ie_um/src/main/java/com/example/ie_um/resource/api/ResourcesController.java
@@ -1,9 +1,15 @@
 package com.example.ie_um.resource.api;
 
+import com.example.ie_um.global.annotation.AuthenticatedId;
 import com.example.ie_um.global.template.RspTemplate;
 import com.example.ie_um.resource.api.dto.request.HashTagReqDto;
 import com.example.ie_um.resource.api.dto.response.ResourceListResDto;
 import com.example.ie_um.resource.application.ResourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +21,22 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/resources")
+@Tag(name = "Resource", description = "자원 추천 관련 API")
 public class ResourcesController {
     private final ResourceService resourceService;
 
+    @Operation(
+            summary = "자원 추천",
+            description = "자원을 추천합니다."
+    )
+    @ApiResponse(
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ResourceListResDto.class)
+            )
+    )
     @PostMapping()
-    public Mono<RspTemplate<ResourceListResDto>> forwardHashtags(@RequestBody HashTagReqDto reqDto) {
+    public Mono<RspTemplate<ResourceListResDto>> forwardHashtags(@AuthenticatedId Long id, @RequestBody HashTagReqDto reqDto) {
         return resourceService.forwardHashtags(reqDto)
                 .map(data -> new RspTemplate<>(
                         HttpStatus.OK,
